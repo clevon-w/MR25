ed52e0e118a5189af05e1ddf337efed4f7a5ba99
Cleon W <54378247+CleonWong@users.noreply.github.com>
2022-05-21 17:53:14 +0800

    navbar looks bootiful now

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,27 +28,27 @@
 // import { logout, reset } from '../features/auth/authSlice'
 
 // function Header() {
-  // const navigate = useNavigate()
-  // const dispatch = useDispatch()
-  // const { user } = useSelector((state) => state.auth)
+// const navigate = useNavigate()
+// const dispatch = useDispatch()
+// const { user } = useSelector((state) => state.auth)
 
-  // const onLogout = () => {
-  //   dispatch(logout())
-  //   dispatch(reset())
-  //   navigate('/')
-  // }
+// const onLogout = () => {
+//   dispatch(logout())
+//   dispatch(reset())
+//   navigate('/')
+// }
 
-  // const toLogin = () => {
-  //   navigate('/login')
-  // }
+// const toLogin = () => {
+//   navigate('/login')
+// }
 
-  // const toRegister = () => {
-  //   navigate('/register')
-  // }
+// const toRegister = () => {
+//   navigate('/register')
+// }
 
-  // const toDashboard = () => {
-  //   navigate('/')
-  // }
+// const toDashboard = () => {
+//   navigate('/')
+// }
 
 //   const { isOpen, onToggle } = useDisclosure()
 
@@ -85,82 +85,81 @@
 //             MR25
 //           </Text>
 
-      //   </Flex>
-      //     {user ? (
-      //       <Stack
-      //         flex={{ base: 1, md: 0 }}
-      //         justify={'flex-end'}
-      //         direction={'row'}
-      //         spacing={6}>
-      //         <Button
-      //           as={'a'}
-      //           fontSize={'sm'}
-      //           fontWeight={400}
-      //           variant={'link'}
-      //           href={'#'}
-      //           onClick={onLogout}>
-      //           Logout
-      //         </Button>
-      //       </Stack>
-      //     ) : (
-      //       <Stack
-      //         flex={{ base: 1, md: 0 }}
-      //         justify={'flex-end'}
-      //         direction={'row'}
-      //         spacing={6}>
-      //         <Button
-      //           as={'a'}
-      //           fontSize={'sm'}
-      //           fontWeight={400}
-      //           variant={'link'}
-      //           href={'#'}
-      //           onClick={toLogin}>
-      //           Sign In
-      //         </Button>
-      //         <Button
-      //           display={{ base: 'none', md: 'inline-flex' }}
-      //           fontSize={'sm'}
-      //           fontWeight={600}
-      //           color={'white'}
-      //           bg={'blue.400'}
-      //           href={'#'}
-      //           _hover={{ bg: 'blue.300' }}
-      //           onClick={toRegister}>
-      //           Sign Up
-      //         </Button>
-      //       </Stack>
-      //     )}
-      // </Flex>
+//   </Flex>
+//     {user ? (
+//       <Stack
+//         flex={{ base: 1, md: 0 }}
+//         justify={'flex-end'}
+//         direction={'row'}
+//         spacing={6}>
+//         <Button
+//           as={'a'}
+//           fontSize={'sm'}
+//           fontWeight={400}
+//           variant={'link'}
+//           href={'#'}
+//           onClick={onLogout}>
+//           Logout
+//         </Button>
+//       </Stack>
+//     ) : (
+//       <Stack
+//         flex={{ base: 1, md: 0 }}
+//         justify={'flex-end'}
+//         direction={'row'}
+//         spacing={6}>
+//         <Button
+//           as={'a'}
+//           fontSize={'sm'}
+//           fontWeight={400}
+//           variant={'link'}
+//           href={'#'}
+//           onClick={toLogin}>
+//           Sign In
+//         </Button>
+//         <Button
+//           display={{ base: 'none', md: 'inline-flex' }}
+//           fontSize={'sm'}
+//           fontWeight={600}
+//           color={'white'}
+//           bg={'blue.400'}
+//           href={'#'}
+//           _hover={{ bg: 'blue.300' }}
+//           onClick={toRegister}>
+//           Sign Up
+//         </Button>
+//       </Stack>
+//     )}
+// </Flex>
 //     </Box>
 //   )
 // }
 
-
 // export default Header
 
-import React, { useState } from "react"
-import { 
-  Link, 
-  Box, 
-  Flex, 
-  Text, 
-  Button, 
-  Stack, 
-  Divider, 
+import React from "react";
+import {
+  Box,
+  Flex,
+  Button,
+  Stack,
+  Divider,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
-  color, } from "@chakra-ui/react"
-import { CloseIcon, HamburgerIcon, AddIcon, ChevronDownIcon } from '@chakra-ui/icons'
-import { useNavigate } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { logout, reset } from '../features/auth/authSlice'
-import Logo from "./Logo"
+  ButtonGroup
+} from "@chakra-ui/react";
+import {
+  CloseIcon,
+  HamburgerIcon,
+  AddIcon,
+  ChevronDownIcon
+} from "@chakra-ui/icons";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
+import Logo from "./Logo";
 
 /**
  * The main header that will be exported.
@@ -168,150 +167,192 @@ import Logo from "./Logo"
  * To add more stuff into the tabs, just add it into the menu links component.
  */
 const Header = (props) => {
-  const [isOpen, setIsOpen] = React.useState(false)
+  const [isOpen, setIsOpen] = React.useState(false);
 
-  const toggle = () => setIsOpen(!isOpen)
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <NavBarContainer {...props}>
-      <Logo w="100px" />
-      <NavBarToggle toggle={toggle} isOpen={isOpen} />
-      <NavBarLinks isOpen={isOpen} />
-      <Divider borderColor={"primary.900"} opacity={1} borderBottomWidth={2} marginTop={6} />
-    </NavBarContainer>
-  )
-}
+    <>
+      <NavBarContainer {...props}>
+        <Logo w="100px" />
+        <NavBarToggle toggle={toggle} isOpen={isOpen} />
+        <NavBarLinks isOpen={isOpen} />
+      </NavBarContainer>
+      <Divider
+        borderColor={"primary.800"}
+        opacity={1}
+        borderBottomWidth={1.5}
+      />
+    </>
+  );
+};
 
 const NavBarToggle = ({ toggle, isOpen }) => {
   return (
     <Box display={{ base: "block", md: "none" }} onClick={toggle}>
-      {isOpen ? <CloseIcon color={"primary.800"} /> : <HamburgerIcon color={"primary.800"} />}
+      {isOpen ? (
+        <CloseIcon color={"primary.800"} />
+      ) : (
+        <HamburgerIcon color={"primary.800"} />
+      )}
     </Box>
-  )
-}
+  );
+};
 
 const NavBarItem = ({ children, isSelected, to, ...rest }) => {
-  const [hover, setHover] = useState(false)
-
   return (
-    <Text 
-      display="block"
-      {...rest}
+    <Button
+      size="md"
+      variant="ghost"
+      color={isSelected ? "primary.800" : "primary.600"}
+      bg={isSelected ? "primary.200" : "transparent"}
       onClick={to}
-      fontSize={'md'}
-      fontWeight={700}
-      color={hover || isSelected ? "primary.800" : "primary.600"}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)} >
-        {children}
-    </Text>
-  )
-}
+      _hover={{
+        color: "primary.800",
+        background: "primary.200"
+      }}
+    >
+      {children}
+    </Button>
+  );
+};
 
 const NavBarLinks = ({ isOpen }) => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const { user } = useSelector((state) => state.auth)
-  
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
   const toDashboard = () => {
-    navigate('/')
-  }
+    navigate("/");
+  };
 
   const toLogin = () => {
-    navigate('/login')
-  }
+    navigate("/login");
+  };
 
   const toRegisterEvent = () => {
-    navigate('/registerEvent')
-  }
+    navigate("/registerEvent");
+  };
 
   const toLeaderboard = () => {
-    navigate('/leaderboard')
-  }
+    navigate("/leaderboard");
+  };
 
   const toAccount = () => {
-    navigate('/myAccount')
-  }
+    navigate("/myAccount");
+  };
 
   const toMyResults = () => {
-    navigate('/myResults')
-  }
+    navigate("/myResults");
+  };
 
   const toUploadResult = () => {
-    navigate('/uploadResult')
-  }
+    navigate("/uploadResult");
+  };
 
   const toLogout = () => {
-    dispatch(logout())
-    dispatch(reset())
-    toDashboard()
-  }
+    dispatch(logout());
+    dispatch(reset());
+    toDashboard();
+  };
 
   return (
     <Box
       display={{ base: isOpen ? "block" : "none", md: "block" }}
-      flexBasis={{ base: "100%", md: "auto" }}>
+      flexBasis={{ base: "100%", md: "auto" }}
+    >
       <Stack
-        spacing={8}
+        spacing={4}
         align="center"
         justify={["center", "space-between", "flex-end", "flex-end"]}
         direction={["column", "row", "row", "row"]}
-        pt={[4, 4, 0, 0]}>
-        <NavBarItem isSelected={window.location.pathname === "/"} to={toDashboard}>About MR25</NavBarItem>
-        <NavBarItem isSelected={window.location.pathname === "/registerEvent"} to={toRegisterEvent}>Register</NavBarItem>
-        <NavBarItem isSelected={window.location.pathname === "/leaderboard"} to={toLeaderboard}>Leaderboard</NavBarItem>
+        pt={[4, 4, 0, 0]}
+      >
+        <ButtonGroup gap="2">
+          <NavBarItem
+            isSelected={window.location.pathname === "/"}
+            to={toDashboard}
+          >
+            About MR25
+          </NavBarItem>
+          <NavBarItem
+            isSelected={window.location.pathname === "/registerEvent"}
+            to={toRegisterEvent}
+          >
+            Register
+          </NavBarItem>
+          <NavBarItem
+            isSelected={window.location.pathname === "/leaderboard"}
+            to={toLeaderboard}
+          >
+            Leaderboard
+          </NavBarItem>
+        </ButtonGroup>
         {user ? (
           <Flex>
             <Stack
               flex={{ base: 1, md: 0 }}
-              justify={'flex-end'}
-              direction={'row'}
-              spacing={6}>
-                <Button
-                  variant="outline"
-                  borderColor={"primary.800"} 
+              justify={"flex-end"}
+              direction={"row"}
+              spacing={6}
+            >
+              <Button
+                variant="outline"
+                borderColor={"primary.800"}
+                opacity={1}
+                borderWidth={2}
+                rightIcon={<AddIcon color={"primary.800"} />}
+                color={"primary.800"}
+                fontSize={"md"}
+                fontWeight={700}
+                onClick={toUploadResult}
+              >
+                Upload result
+              </Button>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  fontSize={"md"}
+                  color={"primary.white"}
+                  bg={"primary.800"}
+                  fontWeight={700}
+                  rightIcon={<ChevronDownIcon />}
+                >
+                  {user.data.firstName + " " + user.data.lastName}
+                </MenuButton>
+                <MenuList
+                  borderColor={"primary.900"}
                   opacity={1}
                   borderWidth={2}
-                  rightIcon={<AddIcon color={'primary.800'} />}
-                  color={'primary.800'}
-                  fontSize={'md'}
-                  fontWeight={700}
-                  onClick={toUploadResult} >
-                    Upload result
-                  </Button>
-                <Menu>
-                  <MenuButton 
-                    as={Button}
-                    fontSize={'md'}
-                    color={'primary.white'}
-                    bg={'primary.800'}
-                    fontWeight={700} 
-                    rightIcon={<ChevronDownIcon />}
-                     >
-                    {user.data.firstName + ' ' + user.data.lastName}
-                  </MenuButton>
-                  <MenuList borderColor={"primary.900"} opacity={1} borderWidth={2}>
-                    <MenuItem onClick={toAccount} color={"primary.900"} >My Account</MenuItem>
-                    <MenuItem onClick={toMyResults} color={"primary.900"} >My Results</MenuItem>
-                    <MenuItem onClick={toLogout} color={"accents.red"} >Logout</MenuItem>
-                  </MenuList>
-                </Menu>
+                >
+                  <MenuItem onClick={toAccount} color={"primary.900"}>
+                    My Account
+                  </MenuItem>
+                  <MenuItem onClick={toMyResults} color={"primary.900"}>
+                    My Results
+                  </MenuItem>
+                  <MenuItem onClick={toLogout} color={"accents.red"}>
+                    Logout
+                  </MenuItem>
+                </MenuList>
+              </Menu>
             </Stack>
           </Flex>
         ) : (
           <Button
-            fontSize={'md'}
+            fontSize={"md"}
             fontWeight={700}
-            color={'primary.white'}
-            bg={'primary.800'}
-            onClick={toLogin}>
+            color={"primary.white"}
+            bg={"primary.800"}
+            onClick={toLogin}
+          >
             Login
           </Button>
         )}
       </Stack>
     </Box>
-  )
-}
+  );
+};
 
 const NavBarContainer = ({ children, ...props }) => {
   return (
@@ -321,14 +362,14 @@ const NavBarContainer = ({ children, ...props }) => {
       justify="space-between"
       wrap="wrap"
       w="100%"
-      mb={8}
       p={8}
       bg={["transparent", "transparent", "transparent", "transparent"]}
       color={["white", "white", "white", "white"]}
-      {...props}>
+      {...props}
+    >
       {children}
     </Flex>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
