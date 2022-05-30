cb999be1c18f51381fc45771ad7372698399aab5
Clevon Wong <clevonwongjy@gmail.com>
2022-05-30 09:52:13 +0800

    updated race instructions

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -14,10 +14,8 @@ import {
   GridItem,
   VStack,
   Image,
-  Link,
   UnorderedList,
   ListItem,
-  OrderedList
 } from "@chakra-ui/react";
 import { formatDateDDMonYYYY } from "../utils/helperFunctions";
 import { useNavigate } from "react-router-dom";
@@ -27,6 +25,7 @@ import { useEffect } from "react";
 import routeMap from "../images/routeMap.jpeg";
 import { FiCalendar, FiMapPin, FiCheckCircle, FiXCircle } from "react-icons/fi";
 import ReadMoreToggle from "../components/ReadMoreToggle";
+import RaceInstructions from "../components/RaceInstructions";
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -151,70 +150,6 @@ function Dashboard() {
         </GridItem>
       </Grid>
 
-      <VStack spacing={4} align={'flex-start'}>
-        <Text fontWeight={700} fontSize={"md"}>
-          Race Instruction (Virtual Segment)
-        </Text>
-        <Text fontWeight={600} fontSize={"sm"}>
-          Before your 1st run - Join this event’s Strava “club” for result verifications
-        </Text>
-        <Text fontWeight={400} fontSize={"sm"} >
-          Visit{" "}
-          <Link
-            color="teal.500"
-            href="https://www.strava.com/clubs/VAMR5km"
-          >
-            www.strava.com/clubs/VAMR5km
-          </Link>{" "}
-          and click the “Request to Join” button.
-        </Text>
-        <VStack spacing={2} align={"flex-start"}>
-          <Text fontWeight={400} fontSize={"sm"} >
-            Important notes:
-          </Text>
-          <UnorderedList px={4} >
-            <ListItem fontWeight={400} fontSize={"sm"} >
-              Please set the name in your Strave app to the name you registered with for this race.
-            </ListItem>
-            <ListItem fontWeight={400} fontSize={"sm"} >
-              Please note that by joining this event’s “club” VA MacRitchie Trail 5km Blended Challenge 2022 on Strava,
-              all your run records will be visible to the organiser for the purpose of result verifications.
-              This Strava “club” will be deleted once the event is over.
-            </ListItem>
-          </UnorderedList>
-        </VStack>
-        <Text fontWeight={600} fontSize={"sm"}>
-          Before every run - Set the Type of Activity in Strava app
-        </Text>
-        <Text fontWeight={400} fontSize={"sm"}>
-          Select “Race” in the Type of Activity in your Strava app before recording every run as only Elapsed Time is accepted.
-          The Strava app’s default Moving Time will not be accepted.
-        </Text>
-
-        <Text fontWeight={600} fontSize={"sm"}>
-          After every run - Save a screenshot and upload result
-        </Text>
-        <OrderedList px={4} >
-          <ListItem fontWeight={400} fontSize={"sm"} >
-            Save a screenshot of the run on Strava app as a proof of your completed run (no submission required at the moment).
-            The screenshot should consist a map displaying the route taken and the 2 information above, your Name and the Elapsed Time.
-          </ListItem>
-          <ListItem fontWeight={400} fontSize={"sm"} >
-            Click on “Upload Result +” to upload your run result based on the data captured by your Strava app.
-          </ListItem>
-        </OrderedList>
-        <VStack spacing={2} align={"flex-start"}>
-          <Text fontWeight={400} fontSize={"sm"} >
-            Important note:
-          </Text>
-          <UnorderedList px={4} >
-            <ListItem fontWeight={400} fontSize={"sm"} >
-              Result must be submitted before 2359hr on the day of run. Failure to do so would render the result invalid.
-            </ListItem>
-          </UnorderedList>
-        </VStack>
-      </VStack>
-
       <VStack spacing={4} align={"flex-start"}>
         <Text fontWeight={700} fontSize={"md"}>
           Route
@@ -255,7 +190,7 @@ function Dashboard() {
 
       <VStack spacing={4} align={"flex-start"}>
         <Text fontWeight={700} fontSize={"md"}>
-          Leaderboard - Inidividual
+          Leaderboard - Individual
         </Text>
         <Text fontWeight={400} fontSize={"sm"}>
           {event.eventDetails.indivLB}
@@ -293,10 +228,13 @@ function Dashboard() {
         </GridItem>
         {/* <GridItem colSpan={2}>
           <Text fontWeight={400} fontSize={"sm"}>
-            {event.eventDetails.finalDescription}
+          {event.eventDetails.finalDescription}
           </Text>
         </GridItem> */}
       </Grid>
+      
+      <RaceInstructions />
+
     </VStack>
   ));
 }
