cc10482cea90f670768b4004a23d94c148466c9b
dobirdrobert <dobirdrobert@users.noreply.github.com>
2022-06-15 13:33:27 +0800

    Instructions for birth date input

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -23,6 +23,7 @@ import {
   Input,
   InputRightElement,
   Icon,
+  FormLabel,
 } from "@chakra-ui/react";
 import { FiEyeOff, FiEye } from "react-icons/fi";
 
@@ -250,6 +251,7 @@ function Register() {
             </FormControl>
 
             <FormControl isRequired>
+              <FormLabel>Birth date</FormLabel>
               <Input
                 name="birthDate"
                 value={formData.birthDate}
@@ -257,7 +259,10 @@ function Register() {
                 type="date"
                 onChange={onChange}
               />
-              <FormHelperText>Birth date</FormHelperText>
+              <FormHelperText>
+                For mobile users, tap on top-left corner of calendar for year
+                and month options.
+              </FormHelperText>
               {/* <DatePicker
                 placeholderText="Birth Date (MM/DD/YYY)"
                 selected={date} // Need this prop to display the selected date in the UI.
