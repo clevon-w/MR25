b3b1df8939d951e30d198850d7bfe99f879dfcf7
Clevon Wong <clevonwongjy@gmail.com>
2022-12-18 08:07:25 +0100

    forget password link

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -287,7 +287,11 @@ exports.forgetPassword = asyncHandler(async (req, res) => {
       text:
         "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
         "Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n" +
-        `http://localhost:3000/api/users/reset/${token}\n\n` +
+        `http://${
+          process.env.NODE_ENV === "production"
+            ? "mr25.herokuapp.com"
+            : "localhost:3000"
+        }/api/users/reset/${token}\n\n` +
         "If you did not request this, please ignore this email and your password will remain unchanged.\n",
     };
 
