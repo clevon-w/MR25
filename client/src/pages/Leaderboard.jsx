a9463a83e7d2a4f375b83c530d9c1515fd3000ee
Clevon Wong <clevonwongjy@gmail.com>
2022-12-31 09:42:14 +0100

    changing text C

--- client/src/pages/Leaderboard.jsx
+++ client/src/pages/Leaderboard.jsx
@@ -315,7 +315,7 @@ function Leaderboard() {
             <AlertIcon color={"accents.blue"} />
             {filterParam.eventFormat === "A"
               ? "The Age Performance Index is an indication of how close the participant is to the extrapolated age standard (100 being equal; >100 - exceeding the age standard) calculated based on the MR25 All-Inclusive 10.5km Trail Performance Index, a performance grading system that factors the runner’s age and gender."
-              : "Check back on 31 Dec 2022 for live results of the finals!"}
+              : "In the event of tie, the ranking shown in this live result shall deem “Pending Ratification” due to the rule of “runner who completed the last run earlier wins” which requires manual calculation."}
           </Alert>
         ) : null}
 
